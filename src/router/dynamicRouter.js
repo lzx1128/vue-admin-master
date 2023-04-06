a95d9bb55f95357324ab67cf6c8ad6030a85fa45
CMY0722 <2920538937@qq.com>
2023-04-06 15:05:56 +0800

    后台管理系统

---
+++ src/router/dynamicRouter.js
@@ -0,0 +1,18 @@
+export default {
+  Layout: () => import('@/views/admin/layout'),
+  Home: () => import('@/views/admin/home'),
+
+  // 系统管理
+  User: () => import('@/views/admin/system/user'),
+  Role: () => import('@/views/admin/system/role'),
+  Notice: () => import('@/views/admin/system/notice'),
+  Menu: () => import('@/views/admin/system/menu'),
+  Dict: () => import('@/views/admin/system/dict'),
+  Dept: () => import('@/views/admin/system/dept'),
+
+  // 文章模块
+  ArticleList: () => import('@/views/admin/blogModel/articleList'),
+  ArticleType: () => import('@/views/admin/blogModel/articleType'),
+  FriendlyLink: () => import('@/views/admin/blogModel/friendlyLink')
+
+}
---
+++ src/views/admin/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-container home">
+    <a class="toBlog" :href="`${origin}/index.html#/front/technique/${id}/all`" target="_blank">我的博客</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: true,
+      origin: '',
+      id: ''
+    }
+  },
+  mounted () {
+    this.origin = window.location.origin
+    this.id = window.btoa(this.$store.state.user.userInfo.user.id)
+    console.log(this.origin, this.id)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .home{
+    .toBlog{
+      color: teal;
+    }
+  }
+</style>
